86f8f2b54ac7681c920bdbf0ed9ca0244fcf8439
venkatapathiraj Ravichandran <rajurraju400@gmail.com>
2021-08-08 12:38:13 -0500

    update l1,192.168.0.109 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNBjdal9EWE3uRuH5Fg3UZyMCc1NyXAtaO86sWMBUch5EUBiI6lYQPyk7wlbiotIM2N8NmPgoW65BZfeZHqsxck=

---
+++ 002-variable.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// first method to declare the variable
+	var num int = 3
+
+	// second method to declare the variable
+	//num := 3
+
+	// third method to declare the variable with data type
+	//var num = 3
+
+	//print the num variable use the fmt function or method
+	fmt.Println("Value of num :", num)
+}
